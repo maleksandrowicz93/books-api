464a49c0a7037edc4e06e3f728e300c2a7149a3d
maleksandrowicz93 <47278917+maleksandrowicz93@users.noreply.github.com>
2019-04-06 16:03:18 +0200

    Add Jackson Library (#7)
    * Added Jackson library
    * Added testing method

--- src/main/java/com/github/maleksandrowicz93/controller/BookController.java
+++ src/main/java/com/github/maleksandrowicz93/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.github.maleksandrowicz93.controller;
 
+import com.github.maleksandrowicz93.pojo.Book;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "{hello: world}";
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
     }
 
 }
