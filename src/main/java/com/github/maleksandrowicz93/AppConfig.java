e4c42aa38bf10b7cdb3be75d36e32211bb2f1a79
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-04-06 10:58:57 +0200

    Add App Configuration
    * Added AppConfig Class
    * Set Prefix and Suffix for views

---
+++ src/main/java/com/github/maleksandrowicz93/AppConfig.java
@@ -0,0 +1,25 @@
+package com.github.maleksandrowicz93;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.github.maleksandrowicz93")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+                new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
